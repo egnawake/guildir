6103f354cef8dacd3984480618b4c74de73c107b
João Inácio <egnawake@gmail.com>
2025-09-17 19:07:55 +0100

    Use Supabase service to fetch data

--- app/routes/guilds.tsx
+++ app/routes/guilds.tsx
@@ -1,18 +1,67 @@
+import { data as routerData } from 'react-router';
 import type { Route } from './+types/guilds';
-import data from '../data';
 import GuildCard from '../guild-card/guild-card';
+import { createClient } from '../supabase.server';
 
-export function loader() {
-  return { guilds: data.guilds.getAll() }; 
+export async function loader({ request }: Route.LoaderArgs) {
+  const { supabase, headers } = createClient(request);
+
+  const { status, data, error } = await supabase.from('guilds').select(`
+    id,
+    name,
+    icon,
+    description,
+    tags (
+      name
+    ),
+    games (
+      name
+    )
+  `);
+
+  if (error) {
+    console.error('Supabase error:', error);
+  }
+
+  const guilds = error === null ? data.map(guild => {
+    const { data: iconData } = supabase.storage
+      .from('guild_icons')
+      .getPublicUrl(guild.icon);
+    const icon = iconData.publicUrl;
+
+    return {
+      ...guild,
+      icon,
+      tags: guild.tags.map(tag => tag.name),
+      games: guild.games.map(game => game.name),
+    };
+  }) : null;
+
+  return routerData({ status, guilds }, { headers });
+}
+
+function Error({ status, message }: { status: number, message: string }) {
+  return (
+    <div>
+      <h2>Error ({status})</h2>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default function Guilds({ loaderData }: Route.ComponentProps) {
   return (
     <>
       <h1>Guilds</h1>
-      {loaderData.guilds.map(guild => (
-        <GuildCard key={guild.id} {...guild} />
-      ))}
+      {loaderData.guilds === null
+        ? <Error
+            status={loaderData.status}
+            message="Something went wrong"
+          />
+        : loaderData.guilds.map(guild => (
+            <GuildCard key={guild.id} {...guild} />
+          ))
+      }
     </>
   );
 }
