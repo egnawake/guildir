701953c9bf6e34fb3f844827769362b930a2070c
João Inácio <egnawake@gmail.com>
2025-09-22 16:05:26 +0100

    Update homepage - add navigation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router";
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Home - Guildir" },
+    { name: "description", content: "Find your next MMO guild" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <nav>
+        <Link to="/guilds">Guilds</Link>
+      </nav>
+    </>
+  );
 }
